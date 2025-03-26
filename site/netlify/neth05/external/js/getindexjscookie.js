e5169200f281d8109b84af0d7ed025bc6f2650eb
Paulfox <ppauloa37@gmail.com>
2025-03-26 19:06:12 -0300

    Create getindexjscookie.js

---
+++ site/netlify/neth05/external/js/getindexjscookie.js
@@ -0,0 +1,14 @@
+var textcont = document.getElementById("textdivname")
+
+var cookieget = function() {
+var name = localStorage.getItem("inputnameiis")
+var mail = localStorage.getItem("inputmailiis")
+var phone = localStorage.getItem("inputphoneiis")
+var subname = localStorage.getItem("inputsubnameiis")
+}
+cookieget()
+var pree = function() {
+textcont.textContent = name;
+
+
+}
